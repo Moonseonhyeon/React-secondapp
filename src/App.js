ec206a962b91c9049bfa3ccc8efaeb6a2d5bcf63
Linda <msh1730@hanmail.net>
2020-07-21 18:24:33 +0900

    jxs문법-className으로 태그찾기(값은 대문자로 구분은 -으로), 재사용안할 디자인 구분해서 style로 css하거나 .css파일에 등록

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./Font.css";
+
+function App() {
+  return (
+    <div>
+      <div className="Font-title">안녕</div>
+      <div style={{ fontSize: "50px", color: "blue" }}>반가워</div>
+    </div>
+  );
+}
+
+export default App;
