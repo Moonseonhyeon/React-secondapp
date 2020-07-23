599fc70f35435c8810e04864e3d27624080a4a25
Linda <msh1730@hanmail.net>
2020-07-23 19:11:00 +0900

    수정버튼, 검색, 배열 중간에 추가, ShouldComponentUpdate(), didmount()-생명주기, 스프레드연산자

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import Post from "./Post";
 import styled from "styled-components";
 
+//스타일 컴포넌트는 무조건 class 밖에!! 이유는 그릴 때 마다 포커스문제가 있다.
+const ContainerBox = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
 class App extends Component {
   state = {
     posts: [
@@ -21,11 +27,6 @@ class App extends Component {
   };
 
   render() {
-    const ContainerBox = styled.div`
-      display: grid;
-      justify-content: center;
-    `;
-
     const add = () => {
       this.setState({
         posts: this.state.posts.concat({ id: 4, title: "제목4" }),
@@ -40,10 +41,24 @@ class App extends Component {
       });
     };
 
+    const update = () => {
+      const data = {
+        id: 2,
+        title: "제목200",
+      };
+
+      this.setState({
+        posts: this.state.posts.map((post) =>
+          post.id === 2 ? { ...post, ...data } : post
+        ),
+      });
+    };
+
     return (
       <ContainerBox>
         <button onClick={add}>추가</button>
         <button onClick={del}>삭제</button>
+        <button onClick={update}>수정</button>
         {this.state.posts.map((post) => {
           return <Post id={post.id} title={post.title} />;
         })}
